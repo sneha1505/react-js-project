33a6e01fcdc77b51850b264ab630093cfac7b2cc
sneha1505 <87608797+sneha1505@users.noreply.github.com>
2021-07-29 16:50:35 +0530

    Add files via upload

---
+++ Boot.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Boot()
+ {
+  return (
+    <div className = "note">
+      <h1>
+       React Js Bootcamp
+      <p>
+        This is the last day of the bootcamp.
+       </p>
+      </h1>
+    </div>
+    
+  );
+}
+export default Boot;
